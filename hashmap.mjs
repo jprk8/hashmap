a4c44364d36e804bc23b8b4cd55ab852d105eef0
John Park <176120986+jprk8@users.noreply.github.com>
2024-10-18 14:17:04 -0700

    Finish HashMap with expanding mechanism

--- hashmap.mjs
+++ hashmap.mjs
@@ -5,189 +5,208 @@
 import LinkedList from './linked-list.mjs';
 
 class HashMap {
-    constructor() {
-        this.arr = [];
-        this.capacity = 16;
-        this.loadFactor = 0.75;
+  constructor() {
+    this.arr = [];
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    let index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error('Trying to access index out of bound');
     }
 
-    hash(key) {
-        let hashCode = 0;
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-            // Apply modulo by capacity on each iteration
-            // to prevent integers from getting too big
-            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    if (!this.arr[index]) {
+      const list = new LinkedList();
+      list.append({ key, value });
+      this.arr[index] = list;
+      if (this.isFull()) this.expand();
+    } else {
+      let cur = this.arr[index].head;
+      while (cur != null) {
+        if (cur.value.key === key) {
+          cur.value.value = value;
+          return;
         }
-        return hashCode;
+        cur = cur.nextNode;
+      }
+      this.arr[index].append({ key, value });
+      if (this.isFull()) this.expand();
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error('Trying to access index out of bound');
     }
 
-    set(key, value) {
-        // First check if a new entry will go over the load factor threshold
-
-        const index = this.hash(key);
-        // Create new linked list if the bucket is empty
-        if (!this.arr[index]) {
-            console.log('Making new linked list in this bucket');
-            const list = new LinkedList();
-            list.append({key, value});
-            this.arr[index] = list;
-        } else {
-            // Iterate to check if a key already exists
-            // IF key exists, update the value
-            // ELSE append to the list
-            let cur = this.arr[index].head;
-            while (cur != null) {
-                if (cur.value.key === key) {
-                    console.log(`Updating the existing key's value`);
-                    cur.value.value = value;
-                    return;
-                }
-                cur = cur.nextNode;
-            }
-            console.log('Appending a new key to the same bucket');
-            this.arr[index].append({key, value});
-        }
+    if (!this.arr[index]) {
+      return null;
     }
 
-    get(key) {
-        const index = this.hash(key);
-        if (!this.arr[index]) {
-            return null;
-        }
+    let cur = this.arr[index].head;
+    while (cur != null) {
+      if (cur.value.key === key) {
+        return cur.value.value;
+      }
+      cur = cur.nextNode;
+    }
+    return null;
+  }
 
-        let cur = this.arr[index].head;
+  has(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error('Trying to access index out of bound');
+    }
+
+    if (!this.arr[index]) {
+      return false;
+    }
+
+    let cur = this.arr[index].head;
+    while (cur != null) {
+      if (cur.value.key === key) {
+        return true;
+      }
+      cur = cur.nextNode;
+    }
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error('Trying to access index out of bound');
+    }
+
+    if (!this.arr[index]) {
+      return false;
+    }
+
+    let cur = this.arr[index].head;
+    let i = 0;
+    while (cur != null) {
+      if (cur.value.key === key) {
+        // remove the key and return true
+        this.arr[index].removeAt(i);
+        return true;
+      }
+      cur = cur.nextNode;
+      i++;
+    }
+
+    return false;
+  }
+
+  length() {
+    let sum = 0;
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.arr[i]) {
+        sum += this.arr[i].size();
+      }
+    }
+
+    return sum;
+  }
+
+  clear() {
+    this.arr = [];
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.arr[i]) {
+        let cur = this.arr[i].head;
         while (cur != null) {
-            if (cur.value.key === key) {
-                return cur.value.value;
-            }
-            cur = cur.nextNode;
+          keysArray.push(cur.value.key);
+          cur = cur.nextNode;
         }
-        return null;
+      }
     }
 
-    has(key) {
-        const index = this.hash(key);
-        if (!this.arr[index]) {
-            return false;
-        }
+    return keysArray;
+  }
 
-        let cur = this.arr[index].head;
+  values() {
+    const valuesArray = [];
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.arr[i]) {
+        let cur = this.arr[i].head;
         while (cur != null) {
-            if (cur.value.key === key) {
-                return true;
-            }
-            cur = cur.nextNode;
+          valuesArray.push(cur.value.value);
+          cur = cur.nextNode;
         }
-        return false;
+      }
     }
 
-    remove(key) {
-        const index = this.hash(key);
-        if (!this.arr[index]) {
-            return false;
-        }
+    return valuesArray;
+  }
 
-        let cur = this.arr[index].head;
-        let i = 0;
+  entries() {
+    const entriesArray = [];
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.arr[i]) {
+        let cur = this.arr[i].head;
         while (cur != null) {
-            if (cur.value.key === key) {
-                // remove the key and return true
-                this.arr[index].removeAt(i);
-                return true;
-            }
-            cur = cur.nextNode;
-            i++;
+          const pair = [cur.value.key, cur.value.value];
+          entriesArray.push(pair);
+          cur = cur.nextNode;
         }
-
-        return false;
+      }
     }
 
-    length() {
-        let sum = 0;
-        for (let i = 0; i < this.capacity; i++) {
-            if (this.arr[i]) {
-                sum += this.arr[i].size();
-            }
-        }
+    return entriesArray;
+  }
 
-        return sum;
+  isFull() {
+    const threshold = this.capacity * this.loadFactor;
+    if (this.length() > threshold) {
+      return true;
     }
+    return false;
+  }
 
-    clear() {
-        this.arr = [];
-        this.capacity = 16;
+  expand() {
+    const original = this.entries();
+    this.capacity *= 2;
+    console.log(`Expanding HashMap capacity to ${this.capacity}`);
+    this.clear();
+    for (const item of original) {
+      this.set(item[0], item[1]);
     }
-
-    keys() {
-        const keysArray = [];
-        for (let i = 0; i < this.capacity; i++) {
-            if (this.arr[i]) {
-                let cur = this.arr[i].head;
-                while (cur != null) {
-                    keysArray.push(cur.value.key);
-                    cur = cur.nextNode;
-                }
-            }
-        }
-
-        return keysArray;
-    }
-
-    values() {
-        const valuesArray = [];
-        for (let i = 0; i < this.capacity; i++) {
-            if (this.arr[i]) {
-                let cur = this.arr[i].head;
-                while (cur != null) {
-                    valuesArray.push(cur.value.value);
-                    cur = cur.nextNode;
-                }
-            }
-        }
-
-        return valuesArray;
-    }
-
-    entries() {
-        const entriesArray = [];
-        for (let i = 0; i < this.capacity; i++) {
-            if (this.arr[i]) {
-                let cur = this.arr[i].head;
-                while (cur != null) {
-                    const pair = [cur.value.key, cur.value.value];
-                    entriesArray.push(pair);
-                    cur = cur.nextNode;
-                }
-            }
-        }
-
-        return entriesArray;
-    }
-
-    isFull() {
-        const threshold = this.capacity * this.loadFactor;
-        if (this.length() > threshold) {
-            return true;
-        }
-        return false;
-    }
+  }
 }
 
 const test = new HashMap();
-console.log(test.hash('John'));
-
-test.set('John', 'Park');
-test.set('john', 'bach');
-test.set('Claire', 'Park');
-test.set('Jaden', 'Park');
-test.set('Caitlyn', 'Park');
-// console.log(test.arr[11]);
-
-console.log(test.get('Jaden'));
-console.log(test.keys());
-console.log(test.length());
-console.log(test.values());
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
 console.log(test.entries());
 
-console.log(test.isFull());
+test.set('kite', 'clear');
+console.log(test.entries());
+
+test.set('moon', 'silver');
+console.log(test.entries());
--- linked-list.mjs
+++ linked-list.mjs
@@ -1,152 +1,154 @@
 export default class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const item = new Node(value);
+    if (this.head === null) {
+      this.head = item;
+      this.tail = item;
+      return;
     }
 
-    append(value) {
-        const item = new Node(value);
-        if (this.head === null) {
-            this.head = item;
-            this.tail = item;
-            return;
-        }
+    this.tail.nextNode = item;
+    this.tail = item;
+  }
 
-        this.tail.nextNode = item;
-        this.tail = item;
+  prepend(value) {
+    const item = new Node(value);
+    if (this.head === null) {
+      this.head = item;
+      this.tail = item;
+      return;
     }
 
-    prepend(value) {
-        const item = new Node(value);
-        if (this.head === null) {
-            this.head = item;
-            this.tail = item;
-            return;
-        }
+    item.nextNode = this.head;
+    this.head = item;
+  }
 
-        item.nextNode = this.head;
-        this.head = item;
+  size() {
+    if (this.head === null) return 0;
+    let i = 1;
+    let cur = this.head;
+    while (cur.nextNode != null) {
+      cur = cur.nextNode;
+      i++;
+    }
+    return i;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    return this.tail;
+  }
+
+  at(index) {
+    if (index === 0) return this.head;
+    if (index > this.size() - 1) {
+      console.log(
+        `Index: ${index} does not exist. (List size is ${this.size()})`
+      );
+      return;
     }
 
-    size() {
-        if (this.head === null) return 0;
-        let i = 1;
-        let cur = this.head;
-        while (cur.nextNode != null) {
-            cur = cur.nextNode;
-            i++;
-        }
+    let cur = this.head;
+    for (let i = 0; i < index; i++) {
+      cur = cur.nextNode;
+    }
+
+    return cur;
+  }
+
+  pop() {
+    let cur = this.head;
+    // go to the second to last element
+    while (cur.nextNode.nextNode != null) {
+      cur = cur.nextNode;
+    }
+
+    cur.nextNode = null;
+    this.tail = cur;
+  }
+
+  contains(value) {
+    let cur = this.head;
+    // Need to iterate through the last element for checking match
+    while (cur != null) {
+      if (cur.value === value) {
+        return true;
+      }
+      cur = cur.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    let cur = this.head;
+    let i = 0;
+    while (cur != null) {
+      if (cur.value === value) {
         return i;
+      }
+      cur = cur.nextNode;
+      i++;
+    }
+    return null;
+  }
+
+  toString() {
+    let cur = this.head;
+    let str = '';
+    while (cur != null) {
+      str += `( ${cur.value} ) -> `;
+      cur = cur.nextNode;
     }
 
-    head() {
-        return this.head;
+    return (str += cur);
+  }
+
+  insertAt(value, index) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
     }
 
-    tail() {
-        return this.tail;
+    let cur = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      cur = cur.nextNode;
     }
 
-    at(index) {
-        if (index === 0) return this.head;
-        if (index > this.size() - 1) {
-            console.log(`Index: ${index} does not exist. (List size is ${this.size()})`);
-            return;
-        }
-        
-        let cur = this.head;
-        for (let i = 0; i < index; i++) {
-            cur = cur.nextNode;
-        }
+    const item = new Node(value, cur.nextNode);
+    cur.nextNode = item;
+  }
 
-        return cur;
+  removeAt(index) {
+    if (index === 0) {
+      this.head = this.head.nextNode;
+      return;
     }
 
-    pop() {
-        let cur = this.head;
-        // go to the second to last element
-        while (cur.nextNode.nextNode != null) {
-            cur = cur.nextNode;
-        }
-
-        cur.nextNode = null;
-        this.tail = cur;
+    let cur = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      cur = cur.nextNode;
     }
 
-    contains(value) {
-        let cur = this.head;
-        // Need to iterate through the last element for checking match
-        while (cur != null) {
-            if (cur.value === value) {
-                return true;
-            }
-            cur = cur.nextNode;
-        }
-        return false;
+    cur.nextNode = cur.nextNode.nextNode;
+
+    // Set the tail back to head when only 1 item is left
+    if (this.head.nextNode === null) {
+      this.tail = this.head;
     }
-
-    find(value) {
-        let cur = this.head;
-        let i = 0;
-        while (cur != null) {
-            if (cur.value === value) {
-                return i;
-            }
-            cur = cur.nextNode;
-            i++;
-        }
-        return null;
-    }
-
-    toString() {
-        let cur = this.head;
-        let str = '';
-        while (cur != null) {
-            str += `( ${cur.value} ) -> `;
-            cur = cur.nextNode;
-        }
-
-        return str += cur;
-    }
-
-    insertAt(value, index) {
-        if (index === 0) {
-            this.prepend(value);
-            return;
-        }
-
-        let cur = this.head;
-        for (let i = 0; i < index - 1; i++) {
-            cur = cur.nextNode;
-        }
-
-        const item = new Node(value, cur.nextNode);
-        cur.nextNode = item;
-    }
-
-    removeAt(index) {
-        if (index === 0) {
-            this.head = this.head.nextNode;
-            return;
-        }
-
-        let cur = this.head;
-        for (let i = 0; i < index - 1; i++) {
-            cur = cur.nextNode;
-        }
-
-        cur.nextNode = cur.nextNode.nextNode;
-
-        // Set the tail back to head when only 1 item is left
-        if (this.head.nextNode === null) {
-            this.tail = this.head;
-        }
-    }
+  }
 }
 
 class Node {
-    constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-    }
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
 }
